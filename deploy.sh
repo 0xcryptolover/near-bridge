2b9d38547bee95ec1ca44f0fa58b8cecbd8d7a74
Cuong Tran <tranquoccuong@outlook.com.vn>
2022-05-23 16:30:51 +0700

    Add proxy interface

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,2 @@
-near deploy --wasmFile target/wasm32-unknown-unknown/release/bridge.wasm --accountId=app.bridge.near --networkId=mainnet --nodeUrl=https://rpc.mainnet.near.org
+near deploy --wasmFile target/wasm32-unknown-unknown/release/bridge.wasm --accountId=bridge.incogntio.near --networkId=mainnet --nodeUrl=https://rpc.mainnet.near.org
+near deploy --wasmFile target/wasm32-unknown-unknown/release/proxy.wasm --accountId=proxy.incognito.near --networkId=mainnet --nodeUrl=https://rpc.mainnet.near.org
